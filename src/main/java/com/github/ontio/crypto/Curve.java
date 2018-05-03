e65fce630e31cf0f16f2fdac34fc471d84aa408f
Matus Zamborsky <zamborsky@gmail.com>
2018-05-03 13:19:14 +0200

    fix Curve name
    Signed-off-by: Matus Zamborsky <zamborsky@gmail.com>

--- src/main/java/com/github/ontio/crypto/Curve.java
+++ src/main/java/com/github/ontio/crypto/Curve.java
@@ -8,7 +8,7 @@ public enum Curve {
     P224(1, "P-224"),
     P256(2, "P-256"),
     P384(3, "P-384"),
-    P512(4, "P-512"),
+    P512(4, "P-521"),
     SM2P256V1(20, "sm2p256v1");
 
     private int label;

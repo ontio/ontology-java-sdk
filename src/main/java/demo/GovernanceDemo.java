72c62d2fa31702f22ed2981cc5e08efa0f577752
lucas7788 <sishsh@163.com>
2018-06-15 17:52:30 +0800

    change account signature in approve of governance

--- src/main/java/demo/GovernanceDemo.java
+++ src/main/java/demo/GovernanceDemo.java
@@ -22,9 +22,8 @@ public class GovernanceDemo {
             sdk = getOntSdk();
 
 
-            System.out.println(sdk.getConnect().getBalance(Address.parse("0000000000000000000000000000000000000007").toBase58()));
-            System.out.println(sdk.nativevm().ong().unclaimOng(Address.parse("0000000000000000000000000000000000000007").toBase58()));
-            System.exit(0);
+//            System.out.println(Account.getGcmDecodedPrivateKey("EyXxszzKh09jszQXMIFJTmbujnojOzYzPU4cC0wOpuegDgVcRFllATQ81zD0Rp8s","passwordtest","AK3YRcRvKrASQ6nTfW48Z4iMZ2sDTDRiMC",Base64.getDecoder().decode("jbwUF7JxgsiJq5QAy5dfug=="),16384,SignatureScheme.SHA256WITHECDSA));
+//            System.exit(0);
             String password = "111111";
             String privatekey1 = "54ca4db481966046b15f8d15ff433e611c49ab8e68a279ebf579e4cfd108196d";
             Account payerAcct = new Account(Helper.hexToBytes(privatekey1),SignatureScheme.SHA256WITHECDSA);
@@ -37,6 +36,8 @@ public class GovernanceDemo {
 
             String prikey = "75de8489fcb2dcaf2ef3cd607feffde18789de7da129b5e97c81e001793cb7cf";
 //            prikey = "523c5fcf74823831756f0bcb3634234f10b3beb1c05595058534577752ad2d9f";
+            String adminPrivateKey = "957419a5ceaf5bd40e83e0fc59e71b0d7fef68149e3ea99f79149afc441549cd";
+            String adminPrivateKey2 = "ca53fa4f53ed175e39da86f4e02cd87638652cdbdcdae594c81d2e2f2f673745";
             Account account = new Account(Helper.hexToBytes(prikey),SignatureScheme.SHA256WITHECDSA);
             Account account8 = new Account(Helper.hexToBytes(privatekey8),SignatureScheme.SHA256WITHECDSA);
             Account account7 = new Account(Helper.hexToBytes(privatekey7),SignatureScheme.SHA256WITHECDSA);
@@ -51,6 +52,8 @@ public class GovernanceDemo {
                 System.out.println(obj);
                 sdk.getWalletMgr().writeWallet();
             }
+//            System.out.println("account:" + account.getAddressU160().toBase58());
+//            System.out.println("account:" + sdk.getConnect().getBalance(account.getAddressU160().toBase58()));
             if(false){
                 sdk.nativevm().ont().sendTransfer(account,account9.getAddressU160().toBase58(),100000000,payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
                 Thread.sleep(6000);
@@ -60,7 +63,7 @@ public class GovernanceDemo {
             }
 
             if(false){
-                Identity identity = sdk.getWalletMgr().createIdentityFromPriKey(password,prikey);
+                Identity identity = sdk.getWalletMgr().createIdentityFromPriKey(password,adminPrivateKey);
                 String txhash = sdk.nativevm().ontId().sendRegister(identity,password,payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
                 Thread.sleep(6000);
                 Object obj = sdk.getConnect().getSmartCodeEvent(txhash);
@@ -69,10 +72,16 @@ public class GovernanceDemo {
             List<Identity> dids = sdk.getWalletMgr().getWallet().getIdentities();
             Identity identity = dids.get(0);
             System.out.println("identity:" + identity.ontid);
-            System.out.println("account9:" + sdk.getConnect().getBalance(account9.getAddressU160().toBase58()));
+            Identity adminOntid = sdk.getWalletMgr().createIdentityFromPriKey(password,adminPrivateKey);
+            Account adminAccount = new Account(Helper.hexToBytes(adminPrivateKey),SignatureScheme.SHA256WITHECDSA);
+            Account adminAccount2 = new Account(Helper.hexToBytes(adminPrivateKey2),SignatureScheme.SHA256WITHECDSA);
+            System.out.println("account:" + sdk.getConnect().getBalance(account.getAddressU160().toBase58()));
+//            System.out.println("adminontid:" + sdk.nativevm().ontId().sendGetDDO(adminOntid.ontid));
+//            System.out.println("account:" + account.getAddressU160().toBase58());
+
             if(false){
                 String contractAddr = "0000000000000000000000000000000000000007";
-                Identity adminOntid = sdk.getWalletMgr().getWallet().getIdentity("did:ont:AazEvfQPcQ2GEFFPLF1ZLwQ7K5jDn81hve");
+//                Identity adminOntid = sdk.getWalletMgr().getWallet().getIdentity("did:ont:AazEvfQPcQ2GEFFPLF1ZLwQ7K5jDn81hve");
 //                String txhash = sdk.nativevm().auth().assignFuncsToRole(adminOntid.ontid,password,adminOntid.controls.get(0).getSalt(),1,contractAddr,"role",new String[]{"registerCandidate"},payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
 //                String txhash = sdk.nativevm().auth().assignOntIdsToRole(adminOntid.ontid,password,adminOntid.controls.get(0).getSalt(),1,contractAddr,"role",new String[]{identity.ontid},payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
 //
@@ -83,35 +92,40 @@ public class GovernanceDemo {
 //                Object obj = sdk.getConnect().getSmartCodeEvent(txhash);
 //                System.out.println(obj);
             }
-            Account account1 = new Account(Helper.hexToBytes(privatekey9),SignatureScheme.SHA256WITHECDSA);
             if(false){
                 sdk.getWalletMgr().importAccount("blDuHRtsfOGo9A79rxnJFo2iOMckxdFDfYe2n6a9X+jdMCRkNUfs4+C4vgOfCOQ5",password,account.getAddressU160().toBase58(),Base64.getDecoder().decode("0hAaO6CT+peDil9s5eoHyw=="));
 
-                String txhash = sdk.nativevm().governance().registerCandidate(account9,Helper.toHexString(account8.serializePublicKey()),100000,identity.ontid,password,identity.controls.get(0).getSalt(),1,payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
-//
+                String txhash = sdk.nativevm().governance().registerCandidate(adminAccount,Helper.toHexString(account7.serializePublicKey()),10000,identity.ontid,password,identity.controls.get(0).getSalt(),1,payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
+//                String txhash = sdk.nativevm().governance().unRegisterCandidate(account,Helper.toHexString(account7.serializePublicKey()),payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
                 Thread.sleep(6000);
                 Object obj = sdk.getConnect().getSmartCodeEvent(txhash);
                 System.out.println(obj);
 
-
             }
-            if(false){
-                Identity adminOntid = sdk.getWalletMgr().getWallet().getIdentity("did:ont:AazEvfQPcQ2GEFFPLF1ZLwQ7K5jDn81hve");
-//                String txhash = sdk.nativevm().governance().approveCandidate(adminOntid.ontid,password,adminOntid.controls.get(0).getSalt(),Helper.toHexString(account8.serializePublicKey()),payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
-//                String txhash = sdk.nativevm().governance().voteForPeer(account9,new String[]{Helper.toHexString(account8.serializePublicKey())},new long[]{100},payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
+//            System.out.println("contract:" + sdk.getConnect().getBalance(Address.parse("0000000000000000000000000000000000000007").toBase58()));
+//            System.out.println("contract:" + Address.parse("0000000000000000000000000000000000000007").toBase58());
+//            System.out.println("contract999:" + sdk.nativevm().ong().unclaimOng(account.getAddressU160().toBase58()));
+//            System.out.println("contract999:" + sdk.nativevm().ong().unclaimOng(Address.parse("0000000000000000000000000000000000000007").toBase58()));
+//            System.out.println("contract:" + Address.parse("0000000000000000000000000000000000000001").toBase58());
+//            System.out.println("addr:" + Helper.toHexString(account.getAddressU160().toArray()));
+            if(true){
+//                Identity adminOntid = sdk.getWalletMgr().getWallet().getIdentity("did:ont:AazEvfQPcQ2GEFFPLF1ZLwQ7K5jDn81hve");
+//                String txhash = sdk.nativevm().governance().approveCandidate(adminAccount2,Helper.toHexString(account7.serializePublicKey()),payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
+                String txhash = sdk.nativevm().governance().voteForPeer(account,new String[]{Helper.toHexString(account7.serializePublicKey())},new long[]{100},payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
 //String txhash = sdk.nativevm().governance().unVoteForPeer(account9,new String[]{Helper.toHexString(account8.serializePublicKey())},new long[]{100},payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
 //                  String txhash = sdk.nativevm().governance().quitNode(account9,Helper.toHexString(account8.serializePublicKey()),payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
-                String txhash = sdk.nativevm().governance().withdraw(account9,new String[]{Helper.toHexString(account8.serializePublicKey())},new long[]{100},payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
+//                String txhash = sdk.nativevm().governance().withdraw(account,new String[]{Helper.toHexString(account7.serializePublicKey())},new long[]{10000},payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
 //                String txhash = sdk.nativevm().governance().commitDpos(adminOntid.ontid,password,adminOntid.controls.get(0).getSalt(),payerAcct,sdk.DEFAULT_GAS_LIMIT,0);
                 Thread.sleep(6000);
                 System.out.println(sdk.getConnect().getSmartCodeEvent(txhash));
-                System.out.println("account9" +sdk.getConnect().getBalance( account9.getAddressU160().toBase58()));
+//                System.out.println("account9" +sdk.getConnect().getBalance( account9.getAddressU160().toBase58()));
             }
             if(true) {
-                System.out.println("account9:" + sdk.getConnect().getBalance(account9.getAddressU160().toBase58()));
+                System.out.println("account:" + sdk.getConnect().getBalance(account.getAddressU160().toBase58()));
                 String res = sdk.nativevm().governance().getPeerPoolMap();
                 JSONObject jsr = JSONObject.parseObject(res);
-                System.out.println(jsr.getString(Helper.toHexString(account8.serializePublicKey())));
+//                System.out.println(Helper.toHexString(account7.serializePublicKey()));
+                System.out.println(jsr.getString(Helper.toHexString(account7.serializePublicKey())));
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -121,8 +135,8 @@ public class GovernanceDemo {
 
 
     public static OntSdk getOntSdk() throws Exception {
-        String ip = "http://127.0.0.1";
-//        String ip = "http://139.219.129.26";
+//        String ip = "http://127.0.0.1";
+        String ip = "http://139.219.128.60";
 //        String ip = "http://139.219.129.55";
 //        String ip = "http://101.132.193.149";
         String restUrl = ip + ":" + "20334";

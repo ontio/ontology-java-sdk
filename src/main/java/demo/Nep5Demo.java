207ce37d542971d9869d529a1d376e85d5f4a2e5
xizho10 <josh_chow@163.com>
2018-06-19 14:37:01 +0800

    fix nep5 demo sign bug

--- src/main/java/demo/Nep5Demo.java
+++ src/main/java/demo/Nep5Demo.java
@@ -56,34 +56,44 @@ public class Nep5Demo {
 
             Account acct = new com.github.ontio.account.Account(Helper.hexToBytes(privatekey0), ontSdk.defaultSignScheme);
             System.out.println("recv:"+acct.getAddressU160().toBase58());
+            System.out.println("acct1:"+acct1.getAddressU160().toBase58());
             if(false) {
                 long gasLimit = ontSdk.neovm().nep5().sendInitGetGasLimit();
                 System.out.println(gasLimit);
-                //String result = ontSdk.neovm().nep5().sendInit(acct,acct,30000,0);
-                //System.out.println(result);
+                String result = ontSdk.neovm().nep5().sendInit(acct,acct,30000,0);
+                System.out.println(result);
                 System.exit(0);
             }
-            long gasLimit =  ontSdk.neovm().nep5().sendTransferGetGasLimit(acct,acct1.getAddressU160().toBase58(), 9000000000L);
-            System.out.println(gasLimit);
-            ontSdk.neovm().nep5().sendTransfer(acct,acct1.getAddressU160().toBase58(),1000000000L,acct,gasLimit,0);
-//            System.exit(0);
-//
-//
-            if(false){ // sender is multi sign addr
-                String sendAddr = null;
-                byte[][] pubkeys = new byte[2][];
-                pubkeys[0] = acct.serializePublicKey();
-                pubkeys[1] = acct2.serializePublicKey();
-                sendAddr  = Address.addressFromMultiPubKeys(2,pubkeys).toBase58();
+            String multiAddr = Address.addressFromMultiPubKeys(2,acct.serializePublicKey(),acct2.serializePublicKey()).toBase58();
+            System.out.println("multiAddr:"+multiAddr);
+            if(false) {
+                long gasLimit = ontSdk.neovm().nep5().sendTransferGetGasLimit(acct, acct1.getAddressU160().toBase58(), 9000000000L);
+                System.out.println(gasLimit);
+                ontSdk.neovm().nep5().sendTransfer(acct, acct1.getAddressU160().toBase58(), 1000000000L, acct, gasLimit, 0);
+                ontSdk.neovm().nep5().sendTransfer(acct, multiAddr, 1000000000L, acct, gasLimit, 0);
+                System.exit(0);
+            }
 
-                Transaction tx = ontSdk.neovm().nep5().makeTransfer(sendAddr,acct1.getAddressU160().toBase58(),1000000000L,acct,gasLimit,0);
+            if(true){ // sender is multi sign addr
+                String balance = ontSdk.neovm().nep5().queryBalanceOf(multiAddr);
+                System.out.println(new BigInteger(Helper.reverse(Helper.hexToBytes(balance))).longValue());
+
+                Transaction tx = ontSdk.neovm().nep5().makeTransfer(multiAddr,acct1.getAddressU160().toBase58(),10000000L,acct,50000,0);
+                ontSdk.addSign(tx,acct);
                 ontSdk.addMultiSign(tx,2,new Account[]{acct,acct2});
                 Object obj = ontSdk.getConnect().sendRawTransactionPreExec(tx.toHexString());
                 System.out.println(obj);
+             //   ontSdk.getConnect().sendRawTransaction(tx.toHexString());
+                System.out.println(tx.hash().toString());
+                System.exit(0);
             }
+
             String balance = ontSdk.neovm().nep5().queryBalanceOf(acct.getAddressU160().toBase58());
             System.out.println(new BigInteger(Helper.reverse(Helper.hexToBytes(balance))).longValue());
+            balance = ontSdk.neovm().nep5().queryBalanceOf(multiAddr);
+            System.out.println(new BigInteger(Helper.reverse(Helper.hexToBytes(balance))).longValue());
             System.exit(0);
+            
             String totalSupply = ontSdk.neovm().nep5().queryTotalSupply();
             System.out.println(new BigInteger(Helper.reverse(Helper.hexToBytes(totalSupply))).longValue());
             System.exit(0);
@@ -117,7 +127,7 @@ public class Nep5Demo {
         wm.setRpc(rpcUrl);
         wm.setRestful(restUrl);
         wm.setDefaultConnect(wm.getRestful());
-        wm.neovm().nep5().setContractAddress("41d1ab82c53b1dfc2a4cb1425c226f430fb7974b");
+        wm.neovm().nep5().setContractAddress(Helper.reverse("fc68268daa780992c3e91997325bbfe1f1992ccf"));
         wm.openWalletFile("nep5.json");
 
 

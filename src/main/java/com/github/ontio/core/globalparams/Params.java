069aca3db0b6d9346a7db5dee18437a3a5f250e3
lucas7788 <sishsh@163.com>
2018-07-05 20:25:40 +0800

    add globalparams support

---
+++ src/main/java/com/github/ontio/core/globalparams/Params.java
@@ -0,0 +1,33 @@
+package com.github.ontio.core.globalparams;
+
+import com.github.ontio.common.Helper;
+import com.github.ontio.io.BinaryReader;
+import com.github.ontio.io.BinaryWriter;
+import com.github.ontio.io.Serializable;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class Params implements Serializable {
+    public Param[] params;
+    public Params(Param[] params) {
+        this.params = params;
+    }
+
+    @Override
+    public void deserialize(BinaryReader reader) throws IOException {
+
+    }
+
+    @Override
+    public void serialize(BinaryWriter writer) throws IOException {
+        long l = params.length;
+        byte[] aa = Helper.BigInt2Bytes(BigInteger.valueOf(l));
+        String bb = Helper.toHexString(aa);
+        writer.writeVarBytes(aa);
+        for(int i=0;i< params.length;i++) {
+            writer.writeVarString(params[i].key);
+            writer.writeVarString(params[i].value);
+        }
+    }
+}

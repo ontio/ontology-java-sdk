4244d0a84a65166ea7b45de9476d91f576effd4f
Esa Harjula <Esa.Harjula@kytera.fi>
2018-10-07 15:31:13 -0700

    Remove unused and faulty imports
    Signed-off-by: Esa Harjula <esa.harjula@kytera.fi>

--- src/main/java/com/github/ontio/sdk/claim/Claim.java
+++ src/main/java/com/github/ontio/sdk/claim/Claim.java
@@ -27,10 +27,7 @@ import com.github.ontio.crypto.Digest;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.github.ontio.crypto.SignatureScheme;
-import com.github.ontio.sdk.wallet.Identity;
-import sun.misc.BASE64Encoder;
 
-import java.security.spec.AlgorithmParameterSpec;
 import java.text.SimpleDateFormat;
 import java.util.*;
 

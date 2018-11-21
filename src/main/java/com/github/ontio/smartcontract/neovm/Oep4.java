892a15bca50f90323e63e54c9bcaca05ec667d8f
lucas7788 <sishsh@163.com>
2018-11-21 14:49:56 +0800

    fix oep4 initpre bug

--- src/main/java/com/github/ontio/smartcontract/neovm/Oep4.java
+++ src/main/java/com/github/ontio/smartcontract/neovm/Oep4.java
@@ -66,12 +66,12 @@ public class Oep4 {
         func.name = "init";
         if(preExec) {
             byte[] params = BuildParams.serializeAbiFunction(func);
-            Transaction tx = sdk.vm().makeInvokeCodeTransaction(getContractAddress(), null, params,null,0, 0);
+            Transaction tx = sdk.vm().makeInvokeCodeTransaction(Helper.reverse(contractAddress), null, params,null,0, 0);
             if (acct != null) {
                 sdk.signTx(tx, new Account[][]{{acct}});
             }
             Object obj = sdk.getConnect().sendRawTransactionPreExec(tx.toHexString());
-            if (Integer.parseInt(((JSONObject) obj).getString("Result")) != 1){
+            if (Integer.parseInt(((JSONObject) obj).getString("State")) != 1){
                 throw new SDKException(ErrorCode.OtherError("sendRawTransaction PreExec error: "+ obj));
             }
             return ((JSONObject) obj).getLong("Gas");

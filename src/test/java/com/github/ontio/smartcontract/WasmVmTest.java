eefa770b823b89f8ac3831cc885c640be08f96e0
lucas7788 <sishsh@163.com>
2020-04-17 16:33:25 +0800

    add support Uint256 support

--- src/test/java/com/github/ontio/smartcontract/WasmVmTest.java
+++ src/test/java/com/github/ontio/smartcontract/WasmVmTest.java
@@ -7,6 +7,8 @@ import com.github.ontio.OntSdkTest;
 import com.github.ontio.account.Account;
 import com.github.ontio.common.Address;
 import com.github.ontio.common.Helper;
+import com.github.ontio.common.UInt256;
+import com.github.ontio.core.scripts.WasmScriptBuilder;
 import com.github.ontio.crypto.SignatureScheme;
 import com.github.ontio.core.payload.DeployWasmCode;
 import com.github.ontio.core.payload.InvokeWasmCode;
@@ -34,6 +36,17 @@ public class WasmVmTest {
         ontSdk.setDefaultConnect(ontSdk.getRestful());
 //        ontSdk.openWalletFile("NeoVmTest.json");
         payer = new Account(Helper.hexToBytes(OntSdkTest.PRIVATEKEY), SignatureScheme.SHA256WITHECDSA);
+    }
+
+    @Test
+    public void makeTxUint256() {
+        List<Object> params = new ArrayList<Object>();
+        UInt256 u = UInt256.parse("2abf22c752d71f8d65b39f3b12159f965602bf22e942e51d08d1023b9a4ab02f");
+        params.add(u);
+        List<Object> params2 = new ArrayList<Object>();
+        params2.add(params);
+        byte[] bs = WasmScriptBuilder.createWasmInvokeCode("b3d448d29ea1c501d4bb90a89517a58f3fd7d469", "test", params2);
+        Assert.assertEquals("69d4d73f8fa51795a890bbd401c5a19ed248d4b3260474657374012fb04a9a3b02d1081de542e922bf0256969f15123b9fb3658d1fd752c722bf2a",Helper.toHexString(bs));
     }
 
     @Test

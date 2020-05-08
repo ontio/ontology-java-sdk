55a2df5044dc0182fbc25c075598fa05101813b0
lucas7788 <sishsh@163.com>
2020-05-08 15:53:19 +0800

    update ontid

---
+++ src/main/java/com/github/ontio/core/ontid/Signer.java
@@ -0,0 +1,26 @@
+package com.github.ontio.core.ontid;
+
+import com.github.ontio.common.Helper;
+import com.github.ontio.io.BinaryReader;
+import com.github.ontio.io.BinaryWriter;
+import com.github.ontio.io.Serializable;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class Signer implements Serializable {
+    public byte[] id;
+    public int index;
+
+    @Override
+    public void deserialize(BinaryReader reader) throws IOException {
+
+    }
+
+    @Override
+    public void serialize(BinaryWriter writer) throws IOException {
+        writer.writeVarBytes(this.id);
+        BigInteger index = BigInteger.valueOf(this.index);
+        writer.writeVarBytes(Helper.BigIntToNeoBytes(index));
+    }
+}

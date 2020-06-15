046dc0066b34b1ac64eca5250270762078fd7f68
qiluge <wangcheng@onchain.com>
2020-06-15 19:20:29 +0800

    complete

--- src/main/java/com/github/ontio/smartcontract/NativeVm.java
+++ src/main/java/com/github/ontio/smartcontract/NativeVm.java
@@ -20,6 +20,7 @@
 package com.github.ontio.smartcontract;
 
 import com.github.ontio.OntSdk;
+import com.github.ontio.ontid.OntId2;
 import com.github.ontio.sdk.manager.ConnectMgr;
 import com.github.ontio.smartcontract.nativevm.*;
 
@@ -32,51 +33,60 @@ public class NativeVm {
     private Governance governance = null;
     private SideChainGovernance sideChainGovernance = null;
     private OntSdk sdk;
-    public NativeVm(OntSdk sdk){
+
+    public NativeVm(OntSdk sdk) {
         this.sdk = sdk;
     }
+
     /**
-     *  get OntAsset Tx
+     * get OntAsset Tx
+     *
      * @return instance
      */
     public Ont ont() {
-        if(ont == null){
+        if (ont == null) {
             ont = new Ont(sdk);
         }
         return ont;
     }
+
     public Ong ong() {
-        if(ong == null){
+        if (ong == null) {
             ong = new Ong(sdk);
         }
         return ong;
     }
-    public OntId ontId(){
-        if (ontId == null){
+
+    public OntId ontId() {
+        if (ontId == null) {
             ontId = new OntId(sdk);
         }
         return ontId;
     }
-    public GlobalParams gParams(){
-        if (globalParams == null){
+
+    public GlobalParams gParams() {
+        if (globalParams == null) {
             globalParams = new GlobalParams(sdk);
         }
         return globalParams;
     }
-    public Auth auth(){
-        if (auth == null){
+
+    public Auth auth() {
+        if (auth == null) {
             auth = new Auth(sdk);
         }
         return auth;
     }
-    public Governance governance(){
-        if (governance == null){
+
+    public Governance governance() {
+        if (governance == null) {
             governance = new Governance(sdk);
         }
         return governance;
     }
-    public SideChainGovernance sideChainGovernance(){
-        if(sideChainGovernance == null){
+
+    public SideChainGovernance sideChainGovernance() {
+        if (sideChainGovernance == null) {
             sideChainGovernance = new SideChainGovernance(sdk);
         }
         return sideChainGovernance;

3f5583a177c7dad4c018ecac04294d4c129fb88d
qiluge <wangcheng@onchain.com>
2020-06-15 14:48:38 +0800

    support claim of new ONTID

--- src/main/java/demo/ClaimRecordTxDemo.java
+++ src/main/java/demo/ClaimRecordTxDemo.java
@@ -27,97 +27,95 @@ public class ClaimRecordTxDemo {
         try {
             OntSdk ontSdk = getOntSdk();
 
-            String password = "111111";
+            String password = "passwordtest";
 
+            com.github.ontio.account.Account payer = ontSdk.getWalletMgr().getAccount(
+                    "AUNB7xQuBVg8hnRfVz9pyAuZQUqPBiDxDF", password);
 
-            com.github.ontio.account.Account issuerAcc = new com.github.ontio.account.Account(SignatureScheme.SHA256WITHECDSA);
-            com.github.ontio.account.Account subjectAcc = new com.github.ontio.account.Account(SignatureScheme.SHA256WITHECDSA);
-            com.github.ontio.account.Account payer = new com.github.ontio.account.Account(SignatureScheme.SHA256WITHECDSA);
-
-            String pwd = "111111";
-            Identity issuerIdentity = ontSdk.getWalletMgr().createIdentity(pwd);
-            Identity subjectIdentity = ontSdk.getWalletMgr().createIdentity(pwd);
-            String txhash = ontSdk.nativevm().ontId().sendRegister(issuerIdentity.ontid,issuerAcc,payer,20000000,0);
-            showEvent(ontSdk, "sendRegister", txhash);
-
-            txhash = ontSdk.nativevm().ontId().sendRegister(subjectIdentity.ontid, subjectAcc,payer,20000000,0);
-            showEvent(ontSdk, "sendRegister", txhash);
+            Identity issuerIdentity = ontSdk.getWalletMgr().getWallet().getIdentity("did:ont:AJ4C9aTYxTGUhEpaZdPjFSqCqzMCqJDRUd");
+            Identity subjectIdentity = ontSdk.getWalletMgr().getWallet().getIdentity("did:ont:AVe4zVZzteo6HoLpdBwpKNtDXLjJBzB9fv");
+//            String txhash = ontSdk.nativevm().ontId().sendRegister(issuerIdentity.ontid, payer, payer, 20000000, 500);
+//            showEvent(ontSdk, "sendRegister", txhash);
+//            txhash = ontSdk.nativevm().ontId().sendRegister(subjectIdentity.ontid, payer, payer, 20000000, 500);
+//            showEvent(ontSdk, "sendRegister", txhash);
 
             Map<String, Object> map = new HashMap<String, Object>();
             map.put("Issuer", issuerIdentity.ontid);
             map.put("Subject", subjectIdentity.ontid);
 
             Map clmRevMap = new HashMap();
-            clmRevMap.put("typ","AttestContract");
-            clmRevMap.put("addr",subjectIdentity.ontid.replace(Common.didont,""));
+            clmRevMap.put("typ", "AttestContract");
+            clmRevMap.put("addr", subjectIdentity.ontid.replace(Common.didont, ""));
 
-            String claim = ontSdk.nativevm().ontId().createOntIdClaim(issuerIdentity.ontid, issuerAcc,"claim:context", map, map,clmRevMap,System.currentTimeMillis()/1000 +100000);
+            ontSdk.neovm().claimRecord().setContractAddress("52df370680de17bc5d4262c446f102a0ee0d6312");
+            String claim = ontSdk.nativevm().ontId().createOntIdClaim(issuerIdentity.ontid, payer,
+                    "claim:context", map, map, clmRevMap, System.currentTimeMillis() / 1000 + 100000);
             System.out.println(claim);
+            String[] claims = claim.split("\\.");
+
+            JSONObject payload = JSONObject.parseObject(new String(Base64.getDecoder().decode(claims[1].getBytes())));
+            String claimId = payload.getString("jti");
+            System.out.println(Helper.toHexString(issuerIdentity.ontid.getBytes()));
+            System.out.println(Helper.toHexString(subjectIdentity.ontid.getBytes()));
+            System.out.println(Helper.toHexString(claimId.getBytes()));
+            System.out.println("ClaimId:" + claimId);
 
             boolean b = ontSdk.nativevm().ontId().verifyOntIdClaim(claim);
             System.out.println(b);
 
-            System.exit(0);
-
-            Account account = ontSdk.getWalletMgr().importAccount("blDuHRtsfOGo9A79rxnJFo2iOMckxdFDfYe2n6a9X+jdMCRkNUfs4+C4vgOfCOQ5","111111","AazEvfQPcQ2GEFFPLF1ZLwQ7K5jDn81hve",Base64.getDecoder().decode("0hAaO6CT+peDil9s5eoHyw=="));
-            AccountInfo info = ontSdk.getWalletMgr().getAccountInfo(account.address,"111111",account.getSalt());
-            com.github.ontio.account.Account account1 = new com.github.ontio.account.Account(Helper.hexToBytes("75de8489fcb2dcaf2ef3cd607feffde18789de7da129b5e97c81e001793cb7cf"),SignatureScheme.SHA256WITHECDSA);
-
-
-            String[] claims = claim.split("\\.");
-
-            JSONObject payload = JSONObject.parseObject(new String(Base64.getDecoder().decode(claims[1].getBytes())));
-
-            System.out.println("ClaimId:" + payload.getString("jti"));
-
-//            ontSdk.neovm().claimRecord().setContractAddress("9a4c79ee4379a0b5d10db03553ca7e61e17a8977");
-            //
-//            String getstatusRes9 = ontSdk.neovm().claimRecord().sendGetStatus(payload.getString("jti"));
-//            System.out.println("getstatusResBytes:" + getstatusRes9);
-
-            String commitHash = ontSdk.neovm().claimRecord().sendCommit(issuerIdentity.ontid,password,issuerIdentity.controls.get(0).getSalt(),subjectIdentity.ontid,payload.getString("jti"),account1,ontSdk.DEFAULT_GAS_LIMIT,0);
+            Account account = ontSdk.getWalletMgr().importAccount(
+                    "blDuHRtsfOGo9A79rxnJFo2iOMckxdFDfYe2n6a9X+jdMCRkNUfs4+C4vgOfCOQ5",
+                    "111111", "AazEvfQPcQ2GEFFPLF1ZLwQ7K5jDn81hve",
+                    Base64.getDecoder().decode("0hAaO6CT+peDil9s5eoHyw=="));
+            System.out.println(account.address);
+            String commitHash = ontSdk.neovm().claimRecord().sendCommit2(issuerIdentity.ontid, password,
+                    issuerIdentity.controls.get(0).getSalt(), subjectIdentity.ontid, claimId, 1,
+                    payer, ontSdk.DEFAULT_GAS_LIMIT, 500);
             System.out.println("commitRes:" + commitHash);
             Thread.sleep(6000);
             Object obj = ontSdk.getConnect().getSmartCodeEvent(commitHash);
             System.out.println(obj);
 
+            String[] credibleOntId = new String[]{
+                    issuerIdentity.ontid, subjectIdentity.ontid
+            };
+            boolean ontIdCredible = ontSdk.nativevm().ontId().verifyClaimOntIdCredible(claim, credibleOntId);
+            System.out.println("claim ontIdCredible: " + ontIdCredible);
+            boolean notExpired = ontSdk.nativevm().ontId().verifyClaimNotExpired(claim);
+            System.out.println("claim notExpired: " + notExpired);
+            boolean signatureValid = ontSdk.nativevm().ontId().verifyClaimSignature(claim);
+            System.out.println("claim signatureValid: " + signatureValid);
+            boolean notRevoked = ontSdk.nativevm().ontId().verifyClaimNotRevoked(claim);
+            System.out.println("claim notRevoked: " + notRevoked);
 
-            System.out.println(Helper.toHexString(issuerIdentity.ontid.getBytes()));
-            System.out.println(Helper.toHexString(subjectIdentity.ontid.getBytes()));
-            System.out.println(Helper.toHexString(payload.getString("jti").getBytes()));
-            System.out.println(payload.getString("jti"));
-
-
-            String getstatusRes = ontSdk.neovm().claimRecord().sendGetStatus(payload.getString("jti"));
-            System.out.println("getstatusResBytes:" + getstatusRes);
+            boolean res = ontSdk.neovm().claimRecord().sendGetStatus2(claimId);
+            System.out.println("before revoke: " + res);
             Thread.sleep(6000);
-
-//            System.exit(0);
-
-            String revokeHash = ontSdk.neovm().claimRecord().sendRevoke(issuerIdentity.ontid,password,issuerIdentity.controls.get(0).getSalt(),payload.getString("jti"),account1,ontSdk.DEFAULT_GAS_LIMIT,0);
-            System.out.println("revokeRes:" + revokeHash);
+            String revokeHash = ontSdk.neovm().claimRecord().sendRevoke2(subjectIdentity.ontid, password,
+                    subjectIdentity.controls.get(0).getSalt(), claimId, 1, payer, ontSdk.DEFAULT_GAS_LIMIT,
+                    500);
+            System.out.println("revokeRes: " + revokeHash);
             Thread.sleep(6000);
             System.out.println(ontSdk.getConnect().getSmartCodeEvent(revokeHash));
+            boolean revoked = ontSdk.neovm().claimRecord().sendGetStatus2(claimId);
+            System.out.println("after revoke: " + revoked);
 
-
-            String getstatusRes2 = ontSdk.neovm().claimRecord().sendGetStatus(payload.getString("jti"));
-
-            System.out.println("getstatusResBytes2:" + getstatusRes2);
-
-            System.exit(0);
-
-
-//            boolean b = ontSdk.getOntIdTx().verifyOntIdClaim(claim);
-//            System.out.println(b);
-
+            String removeHash = ontSdk.neovm().claimRecord().sendRemove2(subjectIdentity.ontid, password,
+                    subjectIdentity.controls.get(0).getSalt(), claimId, 1, payer, ontSdk.DEFAULT_GAS_LIMIT,
+                    500);
+            System.out.println("removeRes: " + removeHash);
+            Thread.sleep(6000);
+            System.out.println(ontSdk.getConnect().getSmartCodeEvent(removeHash));
+            boolean removed = ontSdk.neovm().claimRecord().sendGetStatus2(claimId);
+            System.out.println("after remove: " + removed);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static OntSdk getOntSdk() throws Exception {
-//        String ip = "http://polaris1.ont.io";
-        String ip = "http://127.0.0.1";
+        String ip = "http://polaris1.ont.io";
+//        String ip = "http://127.0.0.1";
 //        String ip = "http://54.222.182.88;
 //        String ip = "http://101.132.193.149";
         String restUrl = ip + ":" + "20334";
@@ -129,12 +127,13 @@ public class ClaimRecordTxDemo {
         wm.setRestful(restUrl);
         wm.setDefaultConnect(wm.getRestful());
 
-        wm.openWalletFile("ClaimRecordTxDemo.json");
+        wm.openWalletFile("wallet.json");
 
         return wm;
     }
 
-    public static void showEvent(OntSdk ontSdk, String methodName, String txHash) throws InterruptedException, SDKException, ConnectorException, IOException {
+    public static void showEvent(OntSdk ontSdk, String methodName, String txHash) throws InterruptedException,
+            SDKException, ConnectorException, IOException {
         System.out.println("methodName: " + methodName + " txHash: " + txHash);
         Thread.sleep(6000);
         System.out.println("event: " + ontSdk.getRestful().getSmartCodeEvent(txHash));

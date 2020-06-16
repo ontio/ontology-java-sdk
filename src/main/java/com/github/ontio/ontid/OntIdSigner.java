3ac2f7323a1b82a88034ebf9b20b13e9dc2abcce
qiluge <wangcheng@onchain.com>
2020-06-16 16:21:50 +0800

    supdate document

--- src/main/java/com/github/ontio/ontid/OntIdSigner.java
+++ src/main/java/com/github/ontio/ontid/OntIdSigner.java
@@ -4,8 +4,14 @@ import com.github.ontio.account.Account;
 
 public class OntIdSigner {
     String ontId;
-    String pubKeyId;
+    String pubKeyId; // pubkey URI
     Account signer;
+
+    public OntIdSigner(String ontId, int pubKeyIndex, Account signer) {
+        this.ontId = ontId;
+        this.pubKeyId = String.format("%s#keys-%s", ontId, pubKeyIndex);
+        this.signer = signer;
+    }
 
     public OntIdSigner(String ontId, String pubKeyId, Account signer) {
         this.ontId = ontId;
